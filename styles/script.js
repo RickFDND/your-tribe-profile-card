33f1ac228e44bd46797c7e3db6a60cf74d7c6dd9
RickFDND <rick.pijnacker@hva.nl>
2024-11-11 09:25:41 +0100

    a

--- styles/script.js
+++ styles/script.js
@@ -1,9 +1,1 @@
-const button = document.querySelector("button");
-
-button.addEventListener("click", updateName);
-
-function updateName() {
-  const name = prompt("Rick");
-  button.textContent = `Meer info: ${name}`;
-}
 
